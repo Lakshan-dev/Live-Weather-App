26e82ddeb63cb70a776dcb113968733582c0f9df
“Lakshan <lakshanmadusanka870@gmail.com>
2023-03-06 19:43:06 +0530

    improve level 1

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: "X-RapidAPI-Key",
+    XRapidAPIKeyHeaderValue: "24ecd688admsh1e95f0095518a4dp162ae8jsn5f68493c3642"
+  };
+  
